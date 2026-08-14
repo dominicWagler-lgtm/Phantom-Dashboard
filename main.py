8374092b365f0ee1870347f92749f3a1ff9bc159
Phantom Dashboard <dominicWagler@gmx.de>
2026-08-14 23:07:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import asyncio
 import os
 
 # ==================== EINSTELLUNGEN ====================
-OWNER_ID = 1523728380476919910            # <-- HIER DEINE DISCORD ID EINTRAGEN!
+OWNER_ID = interaction.user.id
+            # <-- HIER DEINE DISCORD ID EINTRAGEN!
 ROLLE_1_ID = 1537902471177838592                           # ID der Rolle beim Annehmen (oder 0 lassen)
 ROLLE_2_ID = 1537853690076200980                           # Zweite Rolle (optional, sonst 0)
 # =======================================================
